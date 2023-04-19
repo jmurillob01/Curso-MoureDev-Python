aac4b70ff899b66130f1a3c3da0df8cd9b5fc1a9
Javier <javiermb2302@gmail.com>
2023-04-19 16:59:01 +0200

    Comentario Tupla y Sets

--- Principiante/05_Tuples.py
+++ Principiante/05_Tuples.py
@@ -7,7 +7,7 @@ os.system('cls')
 my_tuple = tuple()
 my_other_tuple = ()
 
-my_tuple = (21, 1.56, "Javi", "Murillo")
+my_tuple = (21, 1.56, "Javi", "Murillo") # Pêrmite duplicados
 print(my_tuple)
 print(type(my_tuple))
 
